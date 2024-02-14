0030739d1db01d8589957dba5a65a3d95e7564ff
HarringtonIdahosa <81630373+HarringtonIdahosa@users.noreply.github.com>
2024-02-14 05:42:42 -0500

    Update Parent-Child.c

--- Parent-Child.c
+++ Parent-Child.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+// Function to generate a random number between min and max (inclusive)
+int getRandomNumber(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+
 int main() {
     int num_children;
+    char directory[50];
+
+    printf("Copy and paste the directory of the test cases: ");
+    scanf("%s", directory);
 
     printf("How many child processes do you want (max 25)? ");
     scanf("%d", &num_children);
@@ -15,7 +25,6 @@ int main() {
     }
 
     printf("Parent PID is %d\n", getpid());
-    int child_pids[num_children];
 
     for (int child_number = 1; child_number <= num_children; ++child_number) {
         pid_t pid = fork(); // Create a child process
@@ -28,14 +37,19 @@ int main() {
             // Child process
             printf("Started child %d with PID = %d\n", child_number, getpid());
 
-            execlp("/bin/ls", "ls", NULL);
+            // Determine the test case based on the child process index
+            int test_case = child_number % 5 + 1;
 
-            // If execlp fails
-            fprintf(stderr, "Error in execlp\n");
-            return 1;
-        } else {
-            // Save child PID in the array
-            child_pids[child_number - 1] = pid;
+            // command string based on the selected test case and user-provided directory
+            char command[100];
+            snprintf(command, sizeof(command), "%s/test%d", directory, test_case);
+
+            // Execute the selected test case
+            if (execlp(command, command, NULL) == -1) {
+                // If execlp fails
+                perror("Error in execlp");
+                exit(EXIT_FAILURE);
+            }
         }
     }
 
@@ -44,15 +58,9 @@ int main() {
         int status;
         pid_t child_pid = wait(&status);
 
-        for (int j = 0; j < num_children; ++j) {
-            if (child_pid == child_pids[j]) {
-                // Child has finished
-                printf("Child %d (PID %d) finished\n", j + 1, child_pid);
-                break;
-            }
-        }
+        // Child has finished
+        printf("Child %d (PID %d) finished\n", i + 1, child_pid);
     }
 
     return 0;
 }
-
